ee4f07689c774fa62d1a76a1d0fbbbd169ed3324
cszswx <144928692+cszswx@users.noreply.github.com>
2024-04-01 15:39:39 -0400

    Update ast.ml

--- src/ast.ml
+++ src/ast.ml
@@ -35,6 +35,96 @@ type func_def = {
 
 type program = bind list * func_def list
 
+(* Operators definition *)
+type op = 
+    Add | Sub | Equal | Neq | Less | And | Or 
+
+(* Unary operator definition *)
+type unary_op = Negate
+
+(* Types definition *)
+type typ =
+    Int | Bool
+
+(* Expressions definition *)
+type expr =
+    Literal of int
+  | BoolLit of bool
+  | Id of string
+  | Binop of expr * op * expr
+  | Assign of string * expr
+  | Call of string * expr list
+  | ListExpr of expr list
+  | UnaryOp of unary_op * expr
+
+(* Statements definition *)
+type stmt =
+    Block of stmt list
+  | Expr of expr
+  | Return of expr
+  | If of expr * stmt * stmt
+  | While of expr * stmt
+
+(* Function declaration type *)
+type fdecl = {
+  rtyp: typ;
+  fname: string;
+  formals: (typ * string) list;
+  locals: (typ * string) list;
+  body: stmt list;
+}
+
+(* Program type, consisting of variable declarations and functions *)
+type program = (typ * string) list * fdecl list
+
+(* Pretty-printing functions *)
+
+let string_of_op = function
+    Add -> "+"
+  | Sub -> "-"
+  | Equal -> "=="
+  | Neq -> "!="
+  | Less -> "<"
+  | And -> "&&"
+  | Or -> "||"
+
+let string_of_unary_op = function
+    Negate -> "!"
+
+let rec string_of_expr = function
+    Literal(l) -> string_of_int l
+  | BoolLit(b) -> string_of_bool b
+  | Id(s) -> s
+  | Binop(e1, o, e2) -> string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
+  | Assign(v, e) -> v ^ " = " ^ string_of_expr e
+  | Call(f, el) -> f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
+  | ListExpr(el) -> "[" ^ String.concat ", " (List.map string_of_expr el) ^ "]"
+  | UnaryOp(op, e) -> string_of_unary_op op ^ string_of_expr e
+
+let rec string_of_stmt = function
+    Block(stmts) -> "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
+  | Expr(expr) -> string_of_expr expr ^ ";\n"
+  | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n"
+  | If(e, s1, s2) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
+  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+
+let string_of_typ = function
+    Int -> "int"
+  | Bool -> "bool"
+
+let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
+
+let string_of_fdecl fdecl =
+  string_of_typ fdecl.rtyp ^ " " ^ fdecl.fname ^ "(" ^ 
+  String.concat ", " (List.map (fun (t, id) -> string_of_typ t ^ " " ^ id) fdecl.formals) ^ ")\n{\n" ^
+  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
+  String.concat "" (List.map string_of_stmt fdecl.body) ^ "}\n"
+
+let string_of_program (vars, funcs) =
+  "\n\nParsed program: \n\n" ^
+  String.concat "" (List.map string_of_vdecl vars) ^
+  String.concat "\n" (List.map string_of_fdecl funcs)
+
 (* TODO: Fix Pretty-printing functions for LILY *)
 (* Pretty-printing functions *)
 (* let string_of_op = function
