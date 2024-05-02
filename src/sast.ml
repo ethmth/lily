2ec3450278dbbe0f3a55e08a7c82eec811108776
michaelagary <michaelanicole03@gmail.com>
2024-05-02 14:07:46 -0400

    requested prof help w sast.ml

--- src/sast.ml
+++ src/sast.ml
@@ -4,22 +4,7 @@
 
 open Ast
 
-(* Operators definition *)
-type sop = SPlus | SMinus | STimes | SDivide | SEq | SNeq | SLt | SLeq | SGt | SGeq | SMap | SFilter | SReduce
-
-(* Unary operator definition *)
-type sunary_op = SNegate
-
-(* Types definition *)
-type styp = SInt | SBool | SChar | SFloat | SString | SList of styp
-
-type sbind = styp * string
-
-(* Definition of list operations *)
-type slist_op = 
-  | SElwiseAdd
-
-type sexpr = styp * expr_detail
+type sexpr = typ * expr_detail
 and expr_detail =
     SLitInt of int
   | SLitBool of bool
@@ -27,14 +12,14 @@ and expr_detail =
   | SLitChar of char
   | SLitString of string
   | SId of string
-  | SBinop of sexpr * sop * sexpr
+  | SBinop of sexpr * op * sexpr
   | SCall of string * sexpr list
   | SListExpr of sexpr list
-  | SUnaryOp of sunary_op * sexpr
+  | SUnaryOp of unary_op * sexpr
   | SListLit of sexpr list 
-  | SListInit of string * styp * sexpr list
+  | SListInit of string * typ * sexpr list
   (* | SDeclExpr of styp * string * sexpr *)
-  | SListBinop of sexpr * slist_op * sexpr
+  | SListBinop of sexpr * list_op * sexpr
   | SMethodCall of sexpr * string * sexpr list
   | SMap of sexpr * sexpr       
   | SFilter of sexpr * sexpr    
@@ -48,8 +33,8 @@ and sstmt =
   | SFor of sexpr * sexpr * sstmt list
   | SExprStmt of sexpr
   | SReturn of sexpr
-  | SDecl of styp * string
-  | SDeclAssign of styp * string * sexpr
+  | SDecl of typ * string
+  | SDeclAssign of typ * string * sexpr
   | SIDecl of string
   | SIDeclAssign of string * sexpr
   | SFdecl of sfdecl
@@ -57,46 +42,17 @@ and sstmt =
   | STry of sstmt list * sexn_clause list * sstmt list option
 
 and sexn_clause = {
-  sexn_type: styp option;
+  sexn_type: typ option;
   sexn_var: string option;
   shandler: sstmt list;
 }
 
 and sfdecl = {
-  srtyp: styp;
+  srtyp: typ;
   sfname: string;
-  sparameters: sbind list;
+  sparameters: bind list;
   sstmts: sstmt list;
 }
-
-let string_of_sop = function
-    SPlus -> "+" 
-  | SMinus -> "-"
-  | STimes -> "*"
-  | SDivide -> "/" 
-  | SEq -> "==" 
-  | SNeq -> "!=" 
-  | SLt -> "<" 
-  | SLeq -> "<="
-  | SGt -> ">" 
-  | SGeq -> ">="
-  | SMap -> "=>"       
-  | SFilter -> "=>?"   
-  | SReduce -> "=>/"  
-
-let string_of_sunary_op = function
-    SNegate -> "!"
-
-let string_of_slist_op = function
-  | SElwiseAdd -> ".+"
-
-let string_of_styp = function
-    SInt -> "int"
-  | SBool -> "bool"
-  | SChar -> "char"
-  | SFloat -> "float"
-  | SString -> "string"
-  | SList _ -> "list"
 
 (* Program type, consisting of variable declarations and functions *)
 type sprogram = sstmt list
@@ -117,19 +73,19 @@ let rec string_of_sexpr = function
   | SLitFloat(f) -> string_of_float(f)
   | SLitChar(c) -> "\'" ^ String.make 1 c ^ "\'"
   | SLitString(s) -> "\"" ^ s ^ "\""
-  | SId(s) -> s 
-  | SBinop(e1, o, e2) -> string_of_sexpr e1 ^ " " ^ string_of_sop o ^ " " ^ string_of_sexpr e2
+  | SId(s) -> s
+  | SBinop(e1, o, e2) -> string_of_sexpr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_sexpr e2
   | SCall(f, el) -> f ^ "(" ^ String.concat ", " (List.map string_of_sexpr el) ^ ")"
   | SListExpr(el) -> "[" ^ String.concat ", " (List.map string_of_sexpr el) ^ "]"
-  | SUnaryOp(op, e) -> string_of_sunary_op op ^ string_of_sexpr e
-  | SMethodCall(e,s,el) -> string_of_sexpr e ^ "." ^ s ^ string_of_sexpr (ListExpr(el))
+  | SUnaryOp(op, e) -> string_of_unary_op op ^ string_of_sexpr e
+  | SMethodCall(e,s,el) -> string_of_sexpr e ^ "." ^ s ^ string_of_sexpr (SListExpr(el))
   | SMap(list, func) -> string_of_sexpr list ^ " => " ^ string_of_sexpr func
   | SFilter(list, predicate) -> string_of_sexpr list ^ " =>? " ^ string_of_sexpr predicate
   | SReduce(list, func, init) -> string_of_sexpr list ^ " =>/ " ^ string_of_sexpr func ^ " with " ^ string_of_sexpr init
   | SListLit(el) -> "[" ^ String.concat ", " (List.map string_of_sexpr el) ^ "]"
-  | SListBinop(e1, op, e2) -> string_of_sexpr e1 ^ " " ^ string_of_slist_op op ^ " " ^ string_of_sexpr e2
+  | SListBinop(e1, op, e2) -> string_of_sexpr e1 ^ " " ^ string_of_list_op op ^ " " ^ string_of_sexpr e2
   | SListInit(_, _, _) -> "ListInit" (* Added this line to handle the ListInit case *)
-  (*| SDeclExpr(t, s, e) -> "let " ^ s ^ " : " ^ string_of_styp t ^ " = " ^ string_of_sexpr e*)
+  (*| DeclExpr(t, s, e) -> "let " ^ s ^ " : " ^ string_of_typ t ^ " = " ^ string_of_expr e*)
 
 let rec string_of_sstmt_list (sstmts: sstmt list) (curr_indent) = 
   String.concat "" (List.map (fun local_sstmt -> string_of_sstmt local_sstmt curr_indent) sstmts)
@@ -137,19 +93,19 @@ let rec string_of_sstmt_list (sstmts: sstmt list) (curr_indent) =
 and string_of_sstmt (stmt) (curr_indent) = 
   string_of_sindent curr_indent ^
   match stmt with
-  | SExprStmt(sexpr) -> string_of_sexpr sexpr ^ "\n"
-  | SReturn(sexpr) -> "return " ^ string_of_sexpr sexpr ^ "\n"
-  | SIf(se, ss) -> "if (" ^ string_of_sexpr se ^ "):\n" ^ string_of_sstmt_list ss (curr_indent + 1)
-  | SElif(se, ss) -> "elif (" ^ string_of_sexpr se ^ "):\n" ^ string_of_sstmt_list ss (curr_indent + 1)
-  | SElse(ss) -> "else:\n" ^ string_of_sstmt_list ss (curr_indent + 1)
-  | SWhile(se, ss) -> "while (" ^ string_of_sexpr se ^ "):\n" ^ string_of_sstmt_list ss (curr_indent + 1)
-  | SFor(se1, se2, ss) -> "for (" ^ string_of_sexpr se1 ^ ", " ^ string_of_sexpr se2 ^ "):\n" ^ string_of_sstmt_list ss (curr_indent + 1)
-  | SDecl(t, s) -> "let " ^ s ^ " : " ^ string_of_styp t ^ "\n"
-  | SDeclAssign(t, s, se) -> "let " ^ s ^ " : " ^ string_of_styp t ^ " = " ^ string_of_sexpr se ^ "\n"
+  | SExprStmt(expr) -> string_of_sexpr expr ^ "\n"
+  | SReturn(expr) -> "return " ^ string_of_sexpr expr ^ "\n"
+  | SIf(e, s) -> "if (" ^ string_of_sexpr e ^ "):\n" ^ string_of_sstmt_list s (curr_indent + 1)
+  | SElif(e, s) -> "elif (" ^ string_of_sexpr e ^ "):\n" ^ string_of_sstmt_list s (curr_indent + 1)
+  | SElse(s) -> "else:\n" ^ string_of_sstmt_list s (curr_indent + 1)
+  | SWhile(e, s) -> "while (" ^ string_of_sexpr e ^ "):\n" ^ string_of_sstmt_list s (curr_indent + 1)
+  | SFor(e1, e2, s) -> "for (" ^ string_of_sexpr e1 ^ ", " ^ string_of_sexpr e2 ^ "):\n" ^ string_of_sstmt_list s (curr_indent + 1)
+  | SDecl(t, s) -> "let " ^ s ^ " : " ^ string_of_typ t ^ "\n"
+  | SDeclAssign(t, s, e) -> "let " ^ s ^ " : " ^ string_of_typ t ^ " = " ^ string_of_sexpr e ^ "\n"
   | SIDecl(s) -> "let " ^ s ^ "\n"
-  | SIDeclAssign(s, se) -> "let " ^ s ^ " = " ^ string_of_sexpr se ^ "\n"
-  | SFdecl(sf) -> string_of_sfdecl sf (curr_indent)
-  | SAssign(v, se) -> v ^ " = " ^ string_of_sexpr se ^ "\n"
+  | SIDeclAssign(s, e) -> "let " ^ s ^ " = " ^ string_of_sexpr e ^ "\n"
+  | SFdecl(f) -> string_of_sfdecl f (curr_indent)
+  | SAssign(v, e) -> v ^ " = " ^ string_of_sexpr e ^ "\n"
   | STry(try_block, exn_clauses, finally_block) ->
     "try:\n" ^
     string_of_sstmt_list try_block (curr_indent + 1) ^
@@ -162,17 +118,17 @@ and string_of_sexn_clause indent sexn_clause =
   string_of_sindent indent ^
   "except " ^
   (match sexn_clause.sexn_type with
-    | Some(t) -> "(" ^ string_of_styp t ^ " " ^ (match sexn_clause.sexn_var with Some(v) -> v | None -> "") ^ ") "
+    | Some(t) -> "(" ^ string_of_int t ^ " " ^ (match sexn_clause.sexn_var with Some(v) -> v | None -> "") ^ ") "
     | None -> "") ^
   ":\n" ^ string_of_sstmt_list sexn_clause.shandler (indent + 1)
 
 and string_of_sfdecl sfdecl curr_indent =
   "def " ^ sfdecl.sfname ^ "(" ^ 
-  String.concat ", " (List.map (fun (t, id) ->  id ^ " : " ^ string_of_styp t) sfdecl.sparameters) ^ 
-  ")" ^ " -> " ^ string_of_styp sfdecl.srtyp ^ ":" ^ "\n" ^
+  String.concat ", " (List.map (fun (t, id) ->  id ^ " : " ^ string_of_typ t) sfdecl.sparameters) ^ 
+  ")" ^ " -> " ^ string_of_typ sfdecl.srtyp ^ ":" ^ "\n" ^
   String.concat "" (List.map (fun local_sstmt -> string_of_sstmt local_sstmt (curr_indent + 1)) sfdecl.sstmts)
   ^ "\n"
 
-  let string_of_sprogram (sstmts : sstmt list) =
+let string_of_sprogram (sstmts : sstmt list) =
   "\n\nParsed program: \n\n" ^
   string_of_sstmt_list sstmts 0
